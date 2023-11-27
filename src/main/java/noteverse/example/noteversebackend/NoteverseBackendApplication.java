07939de1ada230cdf70b210a77f4d28ec907f64f
EricTruchot <eric.truchot12@gmail.com>
2023-11-27 15:39:09 +0100

    initial commit

---
+++ src/main/java/noteverse/example/noteversebackend/NoteverseBackendApplication.java
@@ -0,0 +1,13 @@
+package noteverse.example.noteversebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteverseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteverseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/noteverse/example/noteversebackend/NoteverseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package noteverse.example.noteversebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteverseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
